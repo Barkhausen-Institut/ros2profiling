b9e5e1a842f0dd0d25cd75b416d46b30252d7209
Maximilian Matthe <maximilian.matthe@barkhauseninstitut.org>
2020-06-21 22:34:28 +0000

    add more profiling names

--- include/ros2profiling/profiling.h
+++ include/ros2profiling/profiling.h
@@ -8,44 +8,67 @@ extern "C" {
 #include <time.h>
 #include <string.h>
 
-#define PROFIDX_RMW_TAKE_WITH_INFO 0
-#define PROFIDX_RCL_TAKE 1
-#define PROFIDX_TAKE_TYPE_ERASED 2
-#define PROFIDX_RMW_PUBLISH 3
-#define PROFIDX_RCL_PUBLISH 4
-#define PROFIDX_HANDLE_MSG 5
-#define PROFIDX_DO_INTER_PROCESS_PUBLISH 6
+/* #define PROFIDX_RMW_TAKE_WITH_INFO 0 */
+/* #define PROFIDX_RCL_TAKE 1 */
+/* #define PROFIDX_TAKE_TYPE_ERASED 2 */
+/* #define PROFIDX_RMW_PUBLISH 3 */
+/* #define PROFIDX_RCL_PUBLISH 4 */
+/* #define PROFIDX_HANDLE_MSG 5 */
+/* #define PROFIDX_DO_INTER_PROCESS_PUBLISH 6 */
+  
+#define PROFIDX_PUB_RCLCPP_INTERPROCESS_PUBLISH 1
+#define PROFIDX_PUB_RCL_PUBLISH 2
+#define PROFIDX_PUB_RMW_PUBLISH 3
+#define PROFIDX_PUB_DDS_WRITE 4  
 
-static void store_profile(const char* topic_name, void* msg, int profile_index, const char* log_msg) {
 
-  if (strstr(topic_name, "_profile_") == NULL)
-    return;
+#define PROFIDX_SUB_DDS_ONDATA 8
+#define PROFIDX_SUB_RCLCPP_TAKE 9
+#define PROFIDX_SUB_RCL_TAKE 10
+#define PROFIDX_SUB_RMW_TAKE_WITH_INFO 11
+#define PROFIDX_SUB_DDS_TAKE 12
+  
+#define PROFIDX_SUB_RCLCPP_HANDLE 18
 
-  long int ns;
-  uint64_t all;
-  time_t sec;
-  struct timespec spec;
+  static uint64_t get_timestamp() {
+    long int ns;
+    uint64_t all;
+    time_t sec;
+    struct timespec spec;
 
-  clock_gettime(CLOCK_REALTIME, &spec);
-  sec = spec.tv_sec;
-  ns = spec.tv_nsec;
+    clock_gettime(CLOCK_REALTIME, &spec);
+    sec = spec.tv_sec;
+    ns = spec.tv_nsec;
 
-  all = (uint64_t) sec * 1000000000UL + (uint64_t) ns;
-
-  uint64_t* msg_u64 = (uint64_t*)msg;
-  const int data_offset = 4;
-  msg_u64[profile_index + data_offset] = all;
-
-  if (log_msg)
-    printf("%lu %s %s\n", all, topic_name, log_msg);
-  else {
-    printf("%lu %s\n", all, topic_name);
+    all = (uint64_t) sec * 1000000000UL + (uint64_t) ns;
+    
   }
-}
+  
+  static void store_profile(const char* topic_name, void* msg, int profile_index, const char* log_msg, uint64_t* ts) {
+
+    if (strstr(topic_name, "_profile_") == NULL)
+      return;
+
+    uint64_t ts_val;
+    if (ts)
+      ts_val = *ts;
+    else
+      ts_val = get_timestamp();
+
+    uint64_t* msg_u64 = (uint64_t*)msg;
+    const int data_offset = 6;
+    msg_u64[profile_index + data_offset] = ts_val;
+
+    if (log_msg)
+      printf("%lu %s %s\n", ts_val, topic_name, log_msg);
+    else {
+      printf("%lu %s\n", ts_val, topic_name);
+    }
+  }
 
   static uint64_t get_profile(void* msg, int profile_index) {
     uint64_t* msg_u64 = (uint64_t*)msg;
-    const int data_offset = 4;
+    const int data_offset = 6;
     return msg_u64[profile_index + data_offset];
   }
 
