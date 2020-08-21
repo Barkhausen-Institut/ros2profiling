67ca934c1306ee76d3fb31ca7898285fb0cb6d44
Maximilian Matthe <maximilian.matthe@barkhauseninstitut.org>
2020-08-21 09:21:00 +0000

    rename patch script

--- patches/createPatches.py
+++ patches/createPatches.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-"""Do not use this script!
+"""This script goes through all changes that were made in some ROS2
+source directories, and creates a patch for them in the present
+directiory.
 
-This script was used to create patches from the forked ROS2 repos onto a tagged version of ROS.
 """
 
 import subprocess
@@ -17,46 +18,23 @@ def check_output(cmd):
     return subprocess.check_output(cmd, shell=True).decode('utf-8')
 
 def processDirectory(f):
-    authors = """--author="Maximilian*" --author="orola*" --author="mmatthe" """
     thisDir = pathlib.Path(__file__).resolve().parent
     cd = f"cd {thisDir}/../../{f}"
     base = pathlib.PurePath(f).name
     bi_url = f"https://github.com/Barkhausen-Institut/{base}.git"
     destination = f"{thisDir}"
 
-    # 0. reset hard
-    check_call(f"{cd} && git reset --hard")
-    check_call(f"{cd} && git cherry-pick --abort || true")
-
-    # 1. add remote
-    check_call(f"{cd} && git remote rm bi || true")
-    check_call(f"{cd} && git remote add bi {bi_url} || true")
-
-    # 2. fetch
-    check_call(f"{cd} && git fetch bi master")
-
-    # 3. find commits
-    commits = check_output(f"{cd} && git log --reverse --pretty=format:\"%H\" --no-merges {authors} bi/master")
-    num = len(commits.split())
-    commits = " ".join(commits.split())
-    
-
-    # 6. cherry-pick
-    print (f"Cherry-picking {num} commits:")
-    print ("\n".join(commits.split()))
-    check_call(f"{cd} && git cherry-pick -n {commits}")
-
-    # 6. format-patch to somewhere
+    # 1. format-patch to somewhere
     check_call(f"{cd} && git diff HEAD > {destination}/{base}.patch")
     
 def main():
     folders = ["ros2/demos",
-               "ros2/common_interfaces",
+               "ros2/common_interfaces", 
                "ros2/rcl",
                "ros2/rclcpp",
                "ros2/rmw_connext",
-               "ros2/rmw_cyclonedds"
-#               "ros2/rmw_fastrtps",
+               "ros2/rmw_cyclonedds",
+               "ros2/rmw_fastrtps",
 ]
 
     for f in folders:
